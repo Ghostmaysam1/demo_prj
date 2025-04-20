b7f5ec662ad1ad2884789f65af389bde3d047d16
= <recoveryghostmaysam@gmail.com>
2025-04-21 03:07:59 +0330

    chore: not important

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,16 +3,7 @@ import routes from '@router/routes';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes,
-    scrollBehavior(to) {
-        if (to.hash) {
-            return {
-                el: to.hash,
-                behavior: 'smooth',
-            }
-        }
-        return { top: 0 }
-    }
+    routes
 
 })
 
