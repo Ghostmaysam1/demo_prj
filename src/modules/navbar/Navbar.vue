dca450a1b5bc23384c7de726743e461cf9beefc3
= <recoveryghostmaysam@gmail.com>
2025-04-20 18:34:36 +0330

    fix(responsive): make navbar responsive

--- src/modules/navbar/Navbar.vue
+++ src/modules/navbar/Navbar.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useWindowSize } from '@vueuse/core';
 
+const { width } = useWindowSize();
+const isMobile = computed(() => {
+    return width.value < 768;
+});
 </script>
 
 <template>
     <nav class="px-3 sticky top-0 py-2 flex justify-between items-center bg-blue-400/5 border-b border-blue-300/40 backdrop-blur-sm">
         <div class="flex z-10 font-bold items-center gap-2"><img src="@pics/logo.svg" alt="Logo"> نام شرکت</div>
 
-        <div class="absolute left-1/2 transform -translate-x-1/2 z-0">
+
+        <div v-if="!isMobile" class="absolute left-1/2 transform -translate-x-1/2 z-0">
             <ul class="flex text-black/70 font-bold items-center gap-5 justify-around">
                 <li class="cursor-pointer hover:text-black/100 transition-all transition-300">خدمات</li>
                 <li class="cursor-pointer hover:text-black/100 transition-all transition-300">درباره ما</li>
@@ -15,7 +22,7 @@
                 <li class="cursor-pointer hover:text-black/100 transition-all transition-300">ارتباطات</li>
             </ul>
         </div>
-        <div class="z-10">
+        <div v-else class="z-10">
             <img  src="@pics/icons/menu.svg" alt="Menu" class="w-7 cursor-pointer">
         </div>
     </nav>
