2661b45d4719d0a8d359cb8ea1a6568b0c35e545
Ghostmaysam <116579333+Ghostmaysam1@users.noreply.github.com>
2025-04-21 02:02:35 -0700

    UI/navbar (#10)
    * feat(UI): active links
    
    * feat(UI): new navbar theme

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import Navbar from '@modules/navbar/Navbar.vue';
 import Home from '@modules/home/Home.vue';
 </script>
 
 <template>
-    <Navbar />
     <Home />
 </template>
