a66ef646dab08abedaf215bb02c84b05d7a0f02c
= <recoveryghostmaysam@gmail.com>
2025-04-21 10:59:51 +0330

    feat(UI): active links

--- src/modules/navbar/Navbar.vue
+++ src/modules/navbar/Navbar.vue
@@ -6,6 +6,16 @@ const { width } = useWindowSize();
 const isMobile = computed(() => {
     return width.value < 768;
 });
+
+function scrollToEl(el: string) {
+    let element = document.querySelector(el);
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center',
+        });
+    }
+}
 </script>
 
 <template>
@@ -15,11 +25,12 @@ const isMobile = computed(() => {
 
         <div v-if="!isMobile" class="absolute left-1/2 transform -translate-x-1/2 z-0">
             <ul class="flex text-black/70 font-bold items-center gap-5 justify-around">
-                <li class="cursor-pointer hover:text-black/100 transition-all transition-300">خدمات</li>
-                <li class="cursor-pointer hover:text-black/100 transition-all transition-300">درباره ما</li>
-                <li class="cursor-pointer hover:text-black/100 transition-all transition-300">کارکنان</li>
-                <li class="cursor-pointer hover:text-black/100 transition-all transition-300">نظرات</li>
-                <li class="cursor-pointer hover:text-black/100 transition-all transition-300">ارتباطات</li>
+                <li @click="()=> scrollToEl('#header')" class="cursor-pointer hover:text-black/100 transition-all transition-300">اصلی</li>
+                <li @click="()=> scrollToEl('#section2')" class="cursor-pointer hover:text-black/100 transition-all transition-300">خدمات</li>
+                <li @click="()=> scrollToEl('#section3')" class="cursor-pointer hover:text-black/100 transition-all transition-300">درباره ما</li>
+                <li @click="()=> scrollToEl('#section4')" class="cursor-pointer hover:text-black/100 transition-all transition-300">کارکنان</li>
+                <li @click="()=> scrollToEl('#section5')" class="cursor-pointer hover:text-black/100 transition-all transition-300">نظرات</li>
+                <li @click="()=> scrollToEl('#section6')" class="cursor-pointer hover:text-black/100 transition-all transition-300">ارتباطات</li>
             </ul>
         </div>
         <div v-else class="z-10">
