736b515377350d779961870bab8a706a0dbd1050
= <recoveryghostmaysam@gmail.com>
2025-04-21 11:14:45 +0330

    fix(UI): header text location fixed

--- src/modules/home/sections/Header.vue
+++ src/modules/home/sections/Header.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div class="h-screen relative bg-orange-200 overflow-hidden grid grid-cols-10 grid-rows-10">
-        <h3 class="font-bold md:text-xl row-start-3 col-start-2 md:col-span-6 col-span-8">"ما عاشق کدنویسی و ساختن چیزای باحال با تکنولوژی‌های روز دنیا هستیم. از طراحی تا توسعه، کنار شماییم تا ایده‌هات رو به نرم‌افزارهایی سریع، مقیاس‌پذیر و کاربردی تبدیل کنیم."</h3>
+        <h3 class="font-bold md:text-xl col-start-2 row-start-3 md:col-start-2 md:col-span-6 col-span-8">"ما عاشق کدنویسی و ساختن چیزای باحال با تکنولوژی‌های روز دنیا هستیم. از طراحی تا توسعه، کنار شماییم تا ایده‌هات رو به نرم‌افزارهایی سریع، مقیاس‌پذیر و کاربردی تبدیل کنیم."</h3>
         <img src="@pics/men.png" class="absolute  lg:w-auto  max-h-[75%] bottom-0 left-1/2 md:left-1/3 lg:left-1/4 -translate-x-1/2 " alt="me">
     </div>
 </template>
