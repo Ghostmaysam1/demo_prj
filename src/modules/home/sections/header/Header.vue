41b9ef4eab5e489171ae209f6c0cbb932f19d17f
Ghostmaysam <116579333+Ghostmaysam1@users.noreply.github.com>
2025-04-24 12:11:10 -0700

    new (header|navbar) UI `dark` and more `responsive` (#27)
    * fix(icons): fix navbar icons color
    
    * chore(UI): change header main text and ui color to dark
    
    * chore(UI): change header UI (add waves)
    
    * fix(UI): make waves fixed in the header section

--- src/modules/home/sections/header/Header.vue
+++ src/modules/home/sections/header/Header.vue
@@ -4,31 +4,37 @@ import { isMobile } from '@/utils/windowSize';
 </script>
 
 <template>
-    <div class="h-screen relative md:py-[60px] py-[20px] overflow-hidden">
+    <div class="h-screen relative md:px-[60px] px-[20px] md:py-[60px] py-[20px] bg-[#313840] overflow-hidden">
         <Navbar />
         <span class="block h-[70px]"></span> <!-- gap -->
-        <div>
-            <div v-if="isMobile" class="flex flex-col justify-between items-center gap-5">
+        <div class="h-full #bg-slate-50">
+            <div v-if="isMobile" class="flex h-full flex-col text-slate-50 justify-evenly items-center gap-5">
                 <div class="flex flex-col items-center gap-5 text-center">
-                    <h2 class="text-3xl font-bold">برج فناوری؛<br> جایی برای توسعه‌ راه‌حل‌های هوشمند</h2>
-                    <h3 class="font-thin text-lg col-span-3 row-span-3 row-start-5">"ما عاشق کدنویسی و ساختن چیزای باحال با تکنولوژی‌های روز دنیا هستیم. از طراحی تا توسعه، کنار شماییم تا ایده‌هات رو به نرم‌افزارهایی سریع، مقیاس‌پذیر و کاربردی تبدیل کنیم."</h3>
-                    <button class="bg-slate-900 w-full text-slate-50 max-w-[480px] text-xl flex justify-center items-center py-4 rounded-2xl">معرفی</button>
+                    <div class="text-right">
+                        <h2 class="text-3xl font-bold">سلام من <span class="text-[#F4CA39]">امیر اسدی</span> هستم</h2>
+                        <p class="flex items-center gap-2 font-bold"><span class="w-9 h-1 bg-[#F4CA39] block"></span> برنامه نویس</p>
+                    </div>
+                    <h3 class="font-thin text-lg col-span-3 text-justify row-span-3 row-start-5">ما عاشق کدنویسی و ساختن چیزای باحال با تکنولوژی‌های روز دنیا هستیم. از طراحی تا توسعه، کنار شماییم تا ایده‌هات رو به نرم‌افزارهایی سریع، مقیاس‌پذیر و کاربردی تبدیل کنیم.</h3>
                 </div>
-                <img src="/men.png" alt="me" class="h-80 object-cover">
+                <img src="/men.png" alt="me" class=" relative z-50">
             </div>
 
             <div v-else class="grid grid-cols-2 gap-5">
-                <div class="flex flex-col justify-between text-center bg">
+                <div class="flex flex-col justify-between text-center bg text-slate-50">
                     <div class="flex flex-col gap-9">
-                        <h2 class="text-4xl font-bold">برج فناوری؛ جایی برای توسعه‌ راه‌حل‌های هوشمند</h2>
-                        <h3 class="font-thin text-lg col-span-3 row-span-3 row-start-5">"ما عاشق کدنویسی و ساختن چیزای باحال با تکنولوژی‌های روز دنیا هستیم. از طراحی تا توسعه، کنار شماییم تا ایده‌هات رو به نرم‌افزارهایی سریع، مقیاس‌پذیر و کاربردی تبدیل کنیم."</h3>
+                        <div class="text-right">
+                        <h2 class="text-3xl font-bold">سلام من <span class="text-[#F4CA39]">امیر اسدی</span> هستم</h2>
+                        <p class="flex items-center gap-2 font-bold"><span class="w-9 h-1 bg-[#F4CA39] block"></span> برنامه نویس</p>
                     </div>
-                    <button class="bg-slate-900 text-slate-50 text-xl flex justify-center items-center py-4 rounded-2xl">معرفی</button>
+                        <h3 class="font-thin text-lg col-span-3 text-justify row-span-3 row-start-5">ما عاشق کدنویسی و ساختن چیزای باحال با تکنولوژی‌های روز دنیا هستیم. از طراحی تا توسعه، کنار شماییم تا ایده‌هات رو به نرم‌افزارهایی سریع، مقیاس‌پذیر و کاربردی تبدیل کنیم.</h3>
+                    </div>
                 </div>
                 <div class="flex justify-center items-center h-90">
                     <img src="/men.png" alt="me" class="h-[550px] bottom-0 z-50 absolute">
                 </div>
             </div>
         </div>
+        <img src="/wave1.svg" class="absolute z-10 bottom-0 w-full transform translate-x-15">
+        <img src="/wave2.svg" class="absolute z-10 bottom-0 w-full transform translate-x-15">
     </div>
 </template>
