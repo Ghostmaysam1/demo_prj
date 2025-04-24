996c281dd73585e25aed9f5a9c91f1ed5dd23945
= <recoveryghostmaysam@gmail.com>
2025-04-24 22:38:49 +0330

    fix(UI): make waves fixed in the header section

--- src/modules/home/sections/header/Header.vue
+++ src/modules/home/sections/header/Header.vue
@@ -34,7 +34,7 @@ import { isMobile } from '@/utils/windowSize';
                 </div>
             </div>
         </div>
+        <img src="/wave1.svg" class="absolute z-10 bottom-0 w-full transform translate-x-15">
+        <img src="/wave2.svg" class="absolute z-10 bottom-0 w-full transform translate-x-15">
     </div>
-    <img src="/wave1.svg" class="absolute z-10 bottom-0 w-full">
-    <img src="/wave2.svg" class="absolute z-10 bottom-0 w-full">
 </template>
