cb06420a6fc4a332014413f9e95fde746f51a376
Ghostmaysam <116579333+Ghostmaysam1@users.noreply.github.com>
2025-04-25 10:32:24 -0700

    chore(services): remove never used codes (#29)

--- src/modules/home/sections/services/components/ServiceBox.vue
+++ src/modules/home/sections/services/components/ServiceBox.vue
@@ -2,46 +2,24 @@
 import { width } from '@/utils/windowSize';
 const props = defineProps<{
     label: string;
-    color: "white" | "green" | "dark";
-    icon: string;
 }>();
-
-enum Color {
-    white = "#F3F3F3",
-    green = "#B9FF66",
-    dark = "#191A23"
-}
-
-enum Highlight {
-    white = Color.dark,
-    green = Color.white,
-    dark = "#F4CA39"
-}
-
-enum TextColor {
-    white = "#F4CA39",
-    green = Color.dark,
-    dark = Color.dark
-}
 </script>
 
 <template>
-    <div :style="{background: Color[props.color]}" dir="ltr" class="relative w-full hover:shadow-lg service-box border-2 flex justify-between items-center max-w-[600px] shadow-black rounded-lg h-full max-h-[310px]">
+    <div dir="ltr" class="relative bg-dark w-full hover:shadow-lg pr-3 service-box border-2 flex justify-between items-center max-w-[600px] shadow-black rounded-lg h-full max-h-[310px]">
         <img src="/wave1.svg" class="absolute bottom-0 w-full opacity-20">
         <div class="h-full w-full p-4">
             <div class="flex flex-row sm:flex-col justify-between gap-2 h-full">
                 <div class="w-full flex flex-col items-start">
-                    <p v-for="(txt, index) in props.label.split(',')" :key="index" :style="{background: Highlight[props.color], color: TextColor[props.color]}" class="rounded-sm font-medium font-mono px-1 md:text-2xl sm:text-lg text-sm">{{ txt }}</p>
+                    <p v-for="(txt, index) in props.label.split(',')" :key="index" class="rounded-sm font-medium text-dark bg-main font-mono px-1 md:text-2xl sm:text-lg text-sm" v-text="txt"></p>
                 </div>
                 <div class="flex items-center gap-2 cursor-pointer">
-                    <img v-if="props.color == 'green'" src="/icons/arrow-white.svg">
-                    <img v-else src="/icons/arrow-green.svg">
-                    <p :style="{color: props.color == 'dark' ? Color.white : Color.dark}">مطالعه بیشتر</p>
+                    <img src="/icons/arrow-white.svg">
+                    <p class="text-white">مطالعه بیشتر</p>
                 </div>
             </div>
         </div>
-        <!-- <img  :src="props.icon" alt="icon" class="h-[100px]"> -->
-         <p v-if="width > 1100" class="w-full" :style="{color: props.color == 'dark' ? 'white' : 'black'}">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک استلورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
+         <p v-if="width > 1100" class="w-full text-justify text-white">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک استلورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
     </div>
 </template>
 
