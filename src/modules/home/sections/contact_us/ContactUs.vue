4abf08c59b3df8e76678a0630714618234651dc2
Ghostmaysam <116579333+Ghostmaysam1@users.noreply.github.com>
2025-04-28 15:32:24 -0700

    feat(UI): add contact_us section (#33)

--- src/modules/home/sections/contact_us/ContactUs.vue
+++ src/modules/home/sections/contact_us/ContactUs.vue
@@ -1,70 +1,73 @@
 <script setup lang="ts">
-import { reactive, ref, computed } from 'vue'
-import { width } from '@/utils/windowSize'
+import { width } from '@/utils/windowSize';
+import CWL from './components/CWL.vue'
 
-const submit = ref(false);
-
-const form = reactive<{name: string, email: string, message: string}>({
-    name: '',
-    email:'',
-    message:''
-})
-
-const handleSubmit = () => {
-  submit.value = true;
-    if (!form.name || !form.email || !form.message) {
-        return
-    }
-    if (!testEmail.value) {
-        return
-    }
-
-    // Here you can send the form data to your server or API
-    console.log('Form submitted:', form)
-    alert('پیام شما با موفقیت ارسال شد');
-    submit.value = false;
-    form.name = ''
-    form.email = ''
-    form.message = ''
-}
-
-const testEmail = computed(() => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    return emailRegex.test(form.email);
-})
+let communicationWays: {name: string, icon: `/icons/${string}.svg`, content: string}[][] = [
+    [{
+        name: 'ایمیل',
+        icon: '/icons/email.svg',
+        content: 'AmirAsadi@gmail.com'
+    },
+    {
+        name: 'آدرس',
+        icon: '/icons/marker.svg',
+        content: 'AmirAsadi@gmail.com'
+    },
+    {
+        name: 'تلفن',
+        icon: '/icons/phone.svg',
+        content: 'AmirAsadi@gmail.com'
+    },
+    {
+        name: 'گیتهاب',
+        icon: '/icons/github.svg',
+        content: 'AmirAsadi@gmail.com'
+    }],
+    [{
+        name: 'تلگرام',
+        icon: '/icons/telegram.svg',
+        content: 'AmirAsadi@gmail.com'
+    },
+    {
+        name: 'واتساپ',
+        icon: '/icons/whatsapp.svg',
+        content: 'AmirAsadi@gmail.com'
+    },
+    {
+        name: 'لینکدین',
+        icon: '/icons/linkedin.svg',
+        content: 'AmirAsadi@gmail.com'
+    },
+    {
+        name: 'فیسبوک',
+        icon: '/icons/facebook.svg',
+        content: 'AmirAsadi@gmail.com'
+    }]
+]
 </script>
 
 <template>
-  <div class="h-screen flex gap-7 flex-col items-center md:pt-20 sm:pt-16 py-10">
-    <div class="flex">
-      <h3 class="bg-[#B9FF66] text-3xl px-2 rounded-lg">ارتباط با ما</h3>
+  <div class="h-screen flex gap-7 flex-col items-center md:px-10 px-2 md:pt-20 sm:pt-16 py-10">
+    <div class="flex justify-start w-full gap-1">
+        <h3 class="bg-main text-3xl px-2 rounded-lg">خدمات</h3>
+        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
     </div>
 
-    <div class="bg-[#F3F3F3] w-full rounded-4xl h-full pt-[60px] md:px-[100px] sm:px-[80px] px-[30px] pb-[80px] flex justify-between items-center    relative overflow-hidden">
-        <form @submit.prevent="handleSubmit" class="lg:w-2/3 w-full flex flex-col gap-7">
-            <label class="flex flex-col gap-3">
-                <span class="text-2xl">نام</span>
-                <input v-model="form.name" type="text" class="border border-black/80 outline-none rounded-[14px] w-full px-4 py-2" />
-                <p v-if="submit && !form.name" class="text-sm font-thin text-red-600/70">لطفا فیلد را پر کنید</p>
-            </label>
-
-            <label class="flex flex-col gap-3">
-                <span class="text-2xl">ایمیل*</span>
-                <input v-model="form.email" type="text" dir="ltr" class="border border-black/80 outline-none rounded-[14px] w-full px-4 py-2" />
-                <p v-if="submit && !form.email" class="text-sm font-thin text-red-600/70">لطفا فیلد را پر کنید</p>
-                <p v-else-if="submit && !testEmail" class="text-sm font-thin text-red-600/70">ایمیل نا معتبر</p>
-            </label>
-
-            <label class="flex flex-col gap-3">
-                <span class="text-2xl">پیام*</span>
-                <textarea v-model="form.message" type="text" class="border max-h-[140px] h-140px min-h-[70px] border-black/80 outline-none rounded-[14px] w-full px-4 py-2" />
-                <p v-if="submit && !form.message" class="text-sm font-thin text-red-600/70">لطفا فیلد را پر کنید</p>
-            </label>
-
-            <button class="bg-slate-900 cursor-pointer text-slate-50 text-xl flex justify-center items-center py-4 rounded-2xl">ارسال پیام</button>
-        </form>
-
-        <img v-if="width > 1024" src="/illustration/star.svg" class="rotate-180 absolute -left-3 transform -translate-x-1/2">
+    <div class="bg-[#F3F3F3] w-fit h-full gap-2.5 rounded-4xl flex flex-col border border-dark/30 shadow-md">
+        <div class="w-full mt-15 h-14 bg-main/90 flex flex-row-reverse justify-center items-center">
+            <img src="/logo-dark.svg" class="h-[140%] opacity-95">
+            <div class="flex flex-col font-mono leading-6 font-medium text-2xl">
+                <p>Amir</p>
+                <p>Asadi</p>
+            </div>
+        </div>
+        <div class="flex flex-row-reverse justify-between items-center md:gap-30 sm:gap-24 md:px-24 gap-10 px-10 w-full h-full">
+            <template v-if="width > 487">
+                <CWL :communication-ways="communicationWays[0]" />
+                <CWL :communication-ways="communicationWays[1]" />
+            </template>
+            <CWL v-else :communication-ways="[...communicationWays[0], ...communicationWays[1]]" />
+        </div>
     </div>
   </div>
 </template>
