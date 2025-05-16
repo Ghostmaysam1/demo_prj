97313e369def7116d2974af419d9309d9f009ac2
= <recoveryghostmaysam@gmail.com>
2025-05-17 00:56:15 +0330

    feat(UI): change Services section UI

--- src/modules/home/sections/services/components/ServiceBox.vue
+++ src/modules/home/sections/services/components/ServiceBox.vue
@@ -1,25 +1,19 @@
 <script setup lang="ts">
-import { width } from '@/utils/windowSize';
-const props = defineProps<{
+const {label, content} = defineProps<{
     label: string;
+    content: string;
 }>();
 </script>
 
 <template>
-    <div dir="ltr" class="relative bg-dark w-full hover:shadow-lg pr-3 service-box border-2 flex justify-between items-center max-w-[600px] shadow-black rounded-lg h-full max-h-[310px]">
+    <div dir="ltr" class="relative bg-dark w-full overflow-hidden hover:shadow-lg transition-all duration-300 scale-95 md:hover:scale-115 hover:scale-3d p-3 service-box border-2 flex flex-col gap-4 justify-between items-center max-w-[400px] shadow-black rounded-sm min-h-[290px] h-fit">
         <img src="/wave1.svg" class="absolute bottom-0 w-full opacity-20">
-        <div class="h-full w-full p-4">
-            <div class="flex flex-row sm:flex-col justify-between gap-2 h-full">
-                <div class="w-full flex flex-col items-start">
-                    <p v-for="(txt, index) in props.label.split(',')" :key="index" class="rounded-sm font-medium text-dark bg-main font-mono px-1 md:text-2xl sm:text-lg text-sm" v-text="txt"></p>
-                </div>
-                <div class="flex items-center gap-2 cursor-pointer">
-                    <img src="/icons/arrow-white.svg">
-                    <p class="text-white">مطالعه بیشتر</p>
-                </div>
-            </div>
+        <div class="flex gap-2 flex-col w-full items-end">
+            <p v-for="(txt, index) in label.split(',')" :key="index" class="rounded-sm font-medium text-dark bg-main font-mono w-fit px-1 md:text-2xl text-lg" v-text="txt"></p>
         </div>
-         <p v-if="width > 1100" class="w-full text-justify text-white">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک استلورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
+        <div class="w-full h-full rounded-md relative flex justify-center items-center">
+            <p class="text-right text-light" dir="rtl">{{ content }}</p>
+        </div>
     </div>
 </template>
 
