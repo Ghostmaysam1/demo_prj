37407cc74bea4268f2b56d0b6769c145cea32017
Ghostmaysam <116579333+Ghostmaysam1@users.noreply.github.com>
2025-05-16 15:29:15 +0330

    add `Contact Icons` to Header (#34)
    * feat(header): add contact icons to header
    
    * fix(header): fix icon hover size

---
+++ src/modules/home/sections/header/components/Icon.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+    const {img, hover} = defineProps<{img: string, hover?: string}>();
+    const isHover = ref(false);
+    const onMouseOver = ()=>{
+        isHover.value = true;
+    }
+    const onMouseLeave = ()=> {
+        isHover.value = false;
+    }
+</script>
+
+<template>
+    <div class="w-10 h-10 bg-main flex items-center justify-center rounded-full border-2 relative">
+        <img :src="img" class="w-7" @mouseleave="onMouseLeave" @mouseover="onMouseOver">
+        <span v-if="isHover" class="absolute top-12 px-2 border -right-5 rounded bg-main/20 flex justify-center items-center w-max pointer-events-none">
+            <p class="w-max" v-text="hover"></p>
+        </span>
+    </div>
+</template>
