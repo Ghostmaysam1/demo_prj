3dbe719fe1fcbc4d3c3085b524ae8b4cc63075bb
= <recoveryghostmaysam@gmail.com>
2025-05-16 15:58:07 +0330

    fix(header): fix icon hovers in lg monitor size

--- src/modules/home/sections/header/components/Icon.vue
+++ src/modules/home/sections/header/components/Icon.vue
@@ -11,8 +11,8 @@
 </script>
 
 <template>
-    <div class="w-10 h-10 bg-main flex items-center justify-center rounded-full border-2 relative">
-        <img :src="img" class="w-7" @mouseleave="onMouseLeave" @mouseover="onMouseOver">
+    <div class="w-10 z-100 h-10 bg-main flex items-center justify-center rounded-full border-2 relative" @mouseleave="onMouseLeave" @mouseover="onMouseOver">
+        <img :src="img" class="w-7">
         <span v-if="isHover" class="absolute top-12 px-2 border -right-5 rounded bg-main/20 flex justify-center items-center w-max pointer-events-none">
             <p class="w-max" v-text="hover"></p>
         </span>
