50be5cdf3da72b90d5dbf89245199208902c7b58
Ghostmaysam <116579333+Ghostmaysam1@users.noreply.github.com>
2025-06-06 02:16:32 +0330

    fix: responsive contactUs section (#38)

--- src/modules/home/sections/contact_us/ContactUs.vue
+++ src/modules/home/sections/contact_us/ContactUs.vue
@@ -48,12 +48,12 @@ let communicationWays: {name: string, icon: `/icons/${string}.svg`, content: str
 
 <template>
   <div class="h-screen flex gap-7 flex-col items-center md:px-10 px-2 md:pt-20 sm:pt-16 py-10">
-    <div class="flex justify-start w-full gap-1">
-        <h3 class="bg-main text-3xl px-2 rounded-lg">خدمات</h3>
-        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
+    <div class="flex items-center justify-start w-full gap-3">
+        <span class="w-30 h-1 bg-main block"></span> 
+        <p>گفت‌و‌گوی ما اولین قدم برای رشد و موفقیت برندت میشه</p>
     </div>
 
-    <div class="bg-[#F3F3F3] w-fit h-full gap-2.5 rounded-4xl flex flex-col border border-dark/30 shadow-md">
+    <div class="bg-[#F3F3F3] w-[98%] md:w-fit h-full gap-2.5 rounded-4xl flex flex-col border border-dark/30 shadow-md">
         <div class="w-full mt-15 h-14 bg-main/90 flex flex-row-reverse justify-center items-center">
             <img src="/logo-dark.svg" class="h-[140%] opacity-95">
             <div class="flex flex-col font-mono leading-6 font-medium text-2xl">
@@ -61,7 +61,7 @@ let communicationWays: {name: string, icon: `/icons/${string}.svg`, content: str
                 <p>Asadi</p>
             </div>
         </div>
-        <div class="flex flex-row-reverse justify-between items-center md:gap-30 sm:gap-24 md:px-24 gap-10 px-10 w-full h-full">
+        <div class="flex flex-row-reverse justify-center items-center sm:gap-20 md:gap-30 md:px-24 gap-10 px-5 sm:px-10 w-full h-full">
             <template v-if="width > 487">
                 <CWL :communication-ways="communicationWays[0]" />
                 <CWL :communication-ways="communicationWays[1]" />
